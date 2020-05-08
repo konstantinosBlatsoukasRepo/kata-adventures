f720f29d0680a010b73be2d6e9259f53c28e26c6
unknown <konstantinosblatsoukasrepo@gmail.com>
2020-05-08 20:13:45 +0300

    episode1(forth-attempt): XOR everything, crazy stuff

--- src/main/java/episode1/FindTheOdd.java
+++ src/main/java/episode1/FindTheOdd.java
@@ -14,45 +14,13 @@ Input: an array of ints
 Output: an int
 */
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 public class FindTheOdd {
 
     public int solve(int[] input) {
-        Map<Integer, Integer> integersFrequencies = getIntegersFrequencies(input);
-
-        return getTheIntegerWithOddFrequency(integersFrequencies);
-    }
-
-    private int getTheIntegerWithOddFrequency(Map<Integer, Integer> integersFrequencies) {
-        List<Entry<Integer, Integer>> integerWithOddFrequencies = integersFrequencies.entrySet()
-            .stream()
-            .filter(v -> isOdd(v.getValue()))
-            .collect(Collectors.toList());
-
-        return integerWithOddFrequencies.get(0).getKey();
-    }
-
-    private boolean isOdd(int number) {
-        return number % 2 != 0;
-    }
-
-    private Map<Integer, Integer> getIntegersFrequencies(int[] input) {
-        Map<Integer, Integer> integersFrequencies = new HashMap<>();
+        int result = 0;
         for (int i = 0; i < input.length; i++) {
-            int currentValue = input[i];
-
-            if (integersFrequencies.containsKey(currentValue)) {
-                integersFrequencies.computeIfPresent(currentValue, (k, v) -> v + 1);
-            } else {
-                integersFrequencies.computeIfAbsent(currentValue, v -> 1);
-            }
+            result ^= input[i];
         }
-        return integersFrequencies;
+        return result;
     }
 }
